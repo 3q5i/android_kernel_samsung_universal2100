d2e7f6646382ff8990dc57b6ce69ad97acf56111
oscyre1006627 <165604309+oscyre1006627@users.noreply.github.com>
2026-04-04 22:10:49 +0900

    trace: thermal_exynos: Remove duplicate GPU trace
    * already defined in thermal_exynos_gpu.h

--- include/trace/events/thermal_exynos.h
+++ include/trace/events/thermal_exynos.h
@@ -67,51 +67,6 @@ TRACE_EVENT(thermal_exynos_power_cpu_limit,
 		__entry->power)
 );
 
-TRACE_EVENT(thermal_exynos_power_gpu_get_power,
-	TP_PROTO(unsigned long freq, u32 load, u32 dynamic_power, u32 static_power),
-
-	TP_ARGS(freq, load, dynamic_power, static_power),
-
-	TP_STRUCT__entry(
-		__field(unsigned long, freq          )
-		__field(u32,           load )
-		__field(u32,           dynamic_power )
-		__field(u32,           static_power  )
-	),
-
-	TP_fast_assign(
-		__entry->freq = freq;
-		__entry->load = load;
-		__entry->dynamic_power = dynamic_power;
-		__entry->static_power = static_power;
-	),
-
-	TP_printk("freq=%lu load=%d dynamic_power=%d static_power=%d",
-		__entry->freq, __entry->load, __entry->dynamic_power, __entry->static_power)
-);
-
-TRACE_EVENT(thermal_exynos_power_gpu_limit,
-	TP_PROTO(unsigned int freq, unsigned long cdev_state, u32 power),
-
-	TP_ARGS(freq, cdev_state, power),
-
-	TP_STRUCT__entry(
-		__field(unsigned int,  freq      )
-		__field(unsigned long, cdev_state)
-		__field(u32,           power     )
-	),
-
-	TP_fast_assign(
-		__entry->freq = freq;
-		__entry->cdev_state = cdev_state;
-		__entry->power = power;
-	),
-
-	TP_printk("freq=%u cdev_state=%lu power=%u",
-		__entry->freq, __entry->cdev_state,
-		__entry->power)
-);
-
 TRACE_EVENT(thermal_exynos_power_allocator,
 	TP_PROTO(struct thermal_zone_device *tz,
 		 u32 power_range, u32 max_allocatable_power,
