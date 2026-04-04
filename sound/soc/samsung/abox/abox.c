be75c10247ede965f8794f9dc0d549b7b144b867
oscyre1006627 <165604309+oscyre1006627@users.noreply.github.com>
2026-04-04 20:49:14 +0900

    ASoC: samsung: abox: Remove KUnit dependencies

--- sound/soc/samsung/abox/abox.c
+++ sound/soc/samsung/abox/abox.c
@@ -9,11 +9,6 @@
  * published by the Free Software Foundation.
  */
 
-#ifdef CONFIG_SND_SOC_SAMSUNG_AUDIO
-#include <kunit/test.h>
-#include <kunit/mock.h>
-#endif
-
 #include <linux/clk.h>
 #include <linux/io.h>
 #include <linux/module.h>
@@ -4051,9 +4046,3 @@ MODULE_AUTHOR("Gyeongtaek Lee, <gt82.lee@samsung.com>");
 MODULE_DESCRIPTION("Samsung ASoC A-Box Driver");
 MODULE_ALIAS("platform:abox");
 MODULE_LICENSE("GPL");
-
-#ifdef CONFIG_SND_SOC_SAMSUNG_AUDIO
-#ifdef CONFIG_ABOX_TEST
-#include "./kunit_test/abox_test.c"
-#endif
-#endif
